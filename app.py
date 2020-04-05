4a30b25e603b946d4b3306e12e9fea5e1974ff8e
Peter <pc.dean@icloud.com>
2020-04-05 01:16:46 +0100

    update game window title and refactor app.py

--- app.py
+++ app.py
@@ -3,22 +3,14 @@ from pong_game.projectile import Projectile
 from pong_game.player import Player
 from pong_game.window import Window
 
-# General setup
 pygame.init()
+pygame.display.set_caption("Pong Not Pong")
 clock = pygame.time.Clock()
 
-# Setting up the main window
-pygame.display.set_caption("Z~P~ $$$$$$$")
-
 window = Window()
-
-# # Game Rectangles
-# player = pygame.Rect(window.w - 20, window.h/2 -70, 10, 140)
-# opponent = pygame.Rect(10, window.h/2 - 70, 10, 140)
-
 projectile = Projectile()
-player_real = Player(window, (window.w - 20))
-player_cpu = Player(window, 10, 10)
+player1 = Player(window, (window.w - 20))
+player2 = Player(window, 10, 10)
 
 ball = pygame.Rect(window.w/2 - 15, window.h/2 - 15, 30, 30)
 
@@ -26,7 +18,7 @@ bg_color = pygame.Color('grey12')
 orange = (255,165,0)
 
 # Game scores
-player_1 = input("Enter your name: ")
+player1_name = input("Enter your name: ")
 playerA_score = 0
 playerB_score = 0
 
@@ -39,36 +31,36 @@ while True:
 
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_DOWN:
-        player_real.speed += 7
+        player1.speed += 7
       if event.key == pygame.K_UP:
-        player_real.speed -= 7
+        player1.speed -= 7
 
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_DOWN:
-        player_real.speed -= 7
+        player1.speed -= 7
       if event.key == pygame.K_UP:
-        player_real.speed += 7
+        player1.speed += 7
 
     if event.type == pygame.VIDEORESIZE:
       window.resize(event.w, event.h)
-      player_real.update()
-      player_cpu.update()
+      player1.update()
+      player2.update()
 
-  projectile.ball_animation(player_real.position, player_cpu.position, ball, window)
-  player_real.player_animation(player_real.position, window)
-  player_cpu.opponent_ai(player_cpu.position, ball, window)
+  projectile.ball_animation(player1.position, player2.position, ball, window)
+  player1.player_animation(player1.position, window)
+  player2.opponent_ai(player2.position, ball, window)
 
   # Visuals
   window.game_display.fill(bg_color)
-  pygame.draw.rect(window.game_display,orange, player_real.position)
-  pygame.draw.rect(window.game_display,orange, player_cpu.position)
+  pygame.draw.rect(window.game_display,orange, player1.position)
+  pygame.draw.rect(window.game_display,orange, player2.position)
   pygame.draw.ellipse(window.game_display,orange, ball)
   pygame.draw.aaline(window.game_display, orange, (window.w/2, 0), (window.w/2,window.h))
   pygame.draw.aaline(window.game_display, orange, (window.w ,window.score_size), (0,window.score_size))
   font = pygame.font.Font(None, 74)
   text = font.render(str(f"Computer - {playerA_score}"), 1, orange)
   window.game_display.blit(text, (window.w / 6, 25))
-  text = font.render(str(f"{player_1} - {playerB_score}"), 1, orange)
+  text = font.render(str(f"{player1_name} - {playerB_score}"), 1, orange)
   window.game_display.blit(text, ((window.w / 6) * 4, 25))
 
   # Updating the window
