fa9a4231e3e10b2215568c8af46dcb9083c3bd6f
Paulo Azoia <pazoia.gomes@gmail.com>
2020-04-06 12:02:08 +0100

    Created game_play function

--- app.py
+++ app.py
@@ -19,38 +19,40 @@ player1 = Player(window, window.w - 20, player1_name)
 
 player2 = Player(window, 10, 'cpu', 10)
 
+def game_play():
+  while True:
+    # Handling input
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_DOWN:
+          player1.speed += 7
+        if event.key == pygame.K_UP:
+          player1.speed -= 7
+
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_DOWN:
+          player1.speed -= 7
+        if event.key == pygame.K_UP:
+          player1.speed += 7
+
+      if event.type == pygame.VIDEORESIZE:
+        window.resize(event.w, event.h)
+        player1.update_player(window, window.w - 20)
+        player2.update_player(window, 10)
+
+    projectile.ball_animation(player1, player2, ball, window)
+    player1.player_animation(player1.position, window)
+    player2.opponent_ai(player2.position, ball, window)
+
+    window.update_display(player1, player2, ball)
+
+    # Updating the window
+    pygame.display.flip()
+    clock.tick(60)
+
 movement()
-
-while True:
-  # Handling input
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
-
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_DOWN:
-        player1.speed += 7
-      if event.key == pygame.K_UP:
-        player1.speed -= 7
-
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_DOWN:
-        player1.speed -= 7
-      if event.key == pygame.K_UP:
-        player1.speed += 7
-
-    if event.type == pygame.VIDEORESIZE:
-      window.resize(event.w, event.h)
-      player1.update_player(window, window.w - 20)
-      player2.update_player(window, 10)
-
-  projectile.ball_animation(player1, player2, ball, window)
-  player1.player_animation(player1.position, window)
-  player2.opponent_ai(player2.position, ball, window)
-
-  window.update_display(player1, player2, ball)
-
-  # Updating the window
-  pygame.display.flip()
-  clock.tick(60)
+game_play()
