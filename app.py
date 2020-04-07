4be5c256109852f581fe75804898429766deea3b
Paulo Azoia <pazoia.gomes@gmail.com>
2020-04-07 15:56:54 +0100

    Game max scoring implemented
    when a player reaches max score
    game resets to main menu

--- app.py
+++ app.py
@@ -24,25 +24,30 @@ if __name__ == '__main__':
   ball = projectile.position
 
   menu = Menu()
-  menu.main_menu(window)
-
-  player1 = Player(window, window.w - 20, 'Player', menu.player1_paddle_size)
-  player2 = Player(window, 10, 'cpu', 100, 10)
-
-  # player1_name = input("Enter your name: ")
 
   while True:
-    print(f"player1 = {player1.score}, player2 = {player2.score}")
-    position = q.get()
-    move(position, player1, player2)
+    menu.main_menu(window)
 
-    projectile.ball_animation(player1, player2, ball, window)
-    player1.player_animation(player1.position, window)
-    player2.opponent_ai(player2.position, ball, window)
+    player1 = Player(window, window.w - 20, 'Player', menu.player1_paddle_size)
+    player2 = Player(window, 10, 'cpu', 100, 10)
 
-    window.update_display(player1, player2, ball)
+    # player1_name = input("Enter your name: ")
 
-    player1.speed = player1.speed/2
+    while True:
 
-    pygame.display.flip()
-    clock.tick(60)
+      position = q.get()
+      move(position, player1, player2)
+
+      projectile.ball_animation(player1, player2, ball, window)
+      player1.player_animation(player1.position, window)
+      player2.opponent_ai(player2.position, ball, window)
+
+      window.update_display(player1, player2, ball)
+
+      player1.speed = player1.speed/2
+
+      pygame.display.flip()
+      clock.tick(60)
+
+      if player1.score == 2 or player2.score == 2:
+        break
