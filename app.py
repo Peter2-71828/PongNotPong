5a65d88ba00dabe4caef3ffe8a418626e6d8db3a
Peter <pc.dean@icloud.com>
2020-05-26 18:36:15 +0100

    update Menu class to accept initialization argument

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     projectile = Projectile(window)
     ball = projectile.position
 
-    menu = Menu()
+    menu = Menu(window)
 
     menu.main_menu(window)
     menu.score_menu(window)
