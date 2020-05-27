09e2a64a56e4996f5cc32d0eb67ae69807507199
Peter <pc.dean@icloud.com>
2020-05-27 15:00:30 +0100

    remove non functional QUIT

--- pong_game/user_in.py
+++ pong_game/user_in.py
@@ -9,23 +9,23 @@ def move(position, player1, player2, window):
         player1.speed += 7
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        # if event.type == pygame.QUIT:
+        #     pygame.quit()
+        #     sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                player1.speed += 7
-
-            if event.key == pygame.K_UP:
-                player1.speed -= 7
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                player1.speed -= 7
-
-            if event.key == pygame.K_UP:
-                player1.speed += 7
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_DOWN:
+        #         player1.speed += 7
+        #
+        #     if event.key == pygame.K_UP:
+        #         player1.speed -= 7
+        #
+        # if event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_DOWN:
+        #         player1.speed -= 7
+        #
+        #     if event.key == pygame.K_UP:
+        #         player1.speed += 7
 
         if event.type == pygame.VIDEORESIZE:
             window.resize(event.w, event.h)
